61a27cc92ca3f8791a0b9853fa38fe7c23ddbfa6
Chaoranger Lorentz <chaoranger@163.com>
2023-11-03 13:10:57 +0800

    链接了图标

--- main.cpp
+++ main.cpp
@@ -3,8 +3,8 @@
 #include <windows.h>
 #include <opencv2/core.hpp>
 #include <opencv2/highgui.hpp>
-#define width 800
-#define height 600
+#define width 640
+#define height 480
 #define resolution width * height
 int main()
 {
@@ -13,7 +13,7 @@ int main()
 	bool debug = 1;
 	int ch;
 	int max = 10;
-	unsigned int iteration = 50;
+	int iteration = 100;
 	unsigned short* result = (unsigned short*)malloc(sizeof(unsigned short) * resolution);
 	double centre[2] = { 0.0,0.0 };
 	double step = 4.0 / width;
@@ -22,17 +22,22 @@ int main()
 	double* c_re = (double*)malloc(sizeof(double*) * resolution);
 	double* c_im = (double*)malloc(sizeof(double*) * resolution);
 	double* temp = (double*)malloc(sizeof(double*) * resolution);
+	//空指针判断
+	if (z_re == NULL || z_im == NULL || c_re == NULL || c_im == NULL || temp == NULL || result == NULL)
+	{
+		std::cerr << "初始化错误：使用malloc申请内存时返回空指针，可能是因为内存空间不足" << std::endl;
+		return -1;
+	}
 	double delta_time = 0;
 	int times = 0;
 	if (debug == 1)
 	{
-		system("cls");
 		std::cout << "已开启debug" << std::endl << "默认分辨率：" << height << "x" << width << std::endl << "3秒后启动" << std::endl;
 		Sleep(3000);
 	}
-	cv::namedWindow("image", cv::WindowFlags::WINDOW_FULLSCREEN);
-	system("cls");
-	while (true)
+	cv::namedWindow("image", cv::WindowFlags::WINDOW_AUTOSIZE);
+	//system("cls");
+	for (int k = 1; ; k++)
 	{
 		if (skippable == 0)
 		{
@@ -47,10 +52,7 @@ int main()
 				c_im[i] = step * (height / 2 - i / width) + centre[1];
 				result[i] = 0;
 			}
-			delta_time = clock() - delta_time;
-			std::cout << "重置初值Δt=" << delta_time << ';';
 			//迭代
-			delta_time = clock();
 #pragma omp parallel for
 			for (int j = 0; j < resolution; j++)
 			{
@@ -61,7 +63,7 @@ int main()
 						temp[j] = z_re[j] * z_re[j] - z_im[j] * z_im[j] + c_re[j];
 						z_im[j] = 2 * z_re[j] * z_im[j] + c_im[j];
 						z_re[j] = temp[j];
-						if (z_re[j] * z_re[j] + z_im[j] * z_im[j] <= 4)
+						if (z_re[j] * z_re[j] + z_im[j] * z_im[j] <= 65536)
 							result[j]++;
 					}
 				}
@@ -74,14 +76,11 @@ int main()
 				else
 					result[i] *= 65535 / iteration;
 			}
-			delta_time = clock() - delta_time;
-			std::cout << "迭代Δt=" << delta_time << ';';
 			//图像显示
-			delta_time = clock();
 			cv::Mat image(height, width, CV_16UC1, result);
 			cv::imshow("image", image);
 			delta_time = clock() - delta_time;
-			std::cout << "图像显示Δt=" << delta_time << ";放大倍率：" << 1.0/step << std::endl;
+			std::clog << "[info]第" << k << "次耗时Δt=" << delta_time << "，放大倍率：" << 2.0 / (step * width) << std::endl;
 		}
 		//键值检测
 		ch = cv::waitKey(0);    //在键盘敲入字符前程序处于等待状态;获取键值
